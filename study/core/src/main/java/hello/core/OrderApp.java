c0a063e07a3f79a901f6908d5ce61e6c7acc9957
titanfall <qkrwogh7366@gmail.com>
2026-07-19 18:08:33 +0900

    feat(core): 주문·할인 도메인 구현 및 정액 할인 테스트

--- study/core/src/main/java/hello/core/OrderApp.java
+++ study/core/src/main/java/hello/core/OrderApp.java
@@ -1,4 +1,28 @@
 package hello.core;
 
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
 public class OrderApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        String itemName = "itemA";
+        int itemPrice = 10000;
+        Order order = orderService.createOrder(memberId, itemName, itemPrice);
+
+        System.out.println(order);
+//        System.out.println(order.calculatePrice());
+    }
 }
--- study/core/src/test/java/hello/core/order/OrderServiceTest.java
+++ study/core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -1,4 +1,31 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
-class OrderServiceTest {
-  
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    public void createOrder() throws Exception {
+        //given
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        //when
+        String itemName = "itemA";
+        int itemPrice = 10000;
+        Order order = orderService.createOrder(memberId, itemName, itemPrice);
+        //then
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
 }
